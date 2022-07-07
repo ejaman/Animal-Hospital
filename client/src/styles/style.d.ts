a8d601c6fa1de2272cbae0ecd010d057646eed7d
minsoo <minsu2344@naver.com>
2022-07-08 02:26:28 +0900

    [#31]feat: d.ts 파일 누락으로 재커밋

---
+++ client/src/styles/style.d.ts
@@ -0,0 +1,12 @@
+import 'styled-components';
+
+declare module 'styled-components'{
+    export interface DefaultTheme {
+        palette:{
+            beige: string,
+            blue: string,
+            orange: string,
+            peach: string,
+        }
+    }
+}
