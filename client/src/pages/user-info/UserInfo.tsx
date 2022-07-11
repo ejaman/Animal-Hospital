d51038cad17f33beeb240b7c9acda11be75860ff
ejaman <leegm042@gmail.com>
2022-07-11 10:46:15 +0900

    [#56]refector: userInfo 재구현

--- client/src/pages/user-info/UserInfo.tsx
+++ client/src/pages/user-info/UserInfo.tsx
@@ -1,73 +1,17 @@
 import React from "react";
-import styled from "styled-components";
+import {
+  MainContainer,
+  Title,
+  Form,
+  Container,
+  InputLabel,
+  InfoInput,
+  InfoBtn,
+  Divider,
+  DeactivateContainer,
+  DeactiveBtn,
+} from "../../components/InfoForm";
 
-const Title = styled.h1`
-  width: 100%;
-  padding-bottom: 10px;
-  font-size: 2em;
-  font-weight: Bolder;
-`;
-const Form = styled.form`
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  color: rgba(0, 0, 0, 0.85);
-  font-size: 14px;
-  font-variant: tabular-nums;
-  line-height: 1.5715;
-  list-style: none;
-  font-feature-settings: "tnum";
-`;
-const MainContainer = styled.div`
-  max-width: 700px;
-  margin: 2rem auto;
-`;
-const Container = styled.div`
-  border-bottom: 1px solid #ebebeb;
-  margin: 1rem 0rem;
-  padding: 1rem 0rem;
-`;
-const InputLabel = styled.p`
-  font-size: 15px;
-`;
-const InfoInput = styled.input`
-  background: none;
-  border: none;
-  outline: none;
-  color: #a0a0a0;
-`;
-const InfoBtn = styled.button`
-  /* background-color: ${(props) => props.theme.palette.orange}; */
-  background-color: black;
-  color: white;
-  border: none;
-  border-radius: 5px;
-  padding: 0.3rem 0.6rem;
-  &:hover {
-    opacity: 0.8;
-  }
-`;
-const Divider = styled.div`
-  margin-top: 1rem;
-`;
-const DeactivateContainer = styled.div`
-  margin: 4rem;
-  padding: 1rem;
-  text-align: center;
-  font-size: 13px;
-`;
-const DeactiveBtn = styled.button`
-  margin-top: 1rem;
-  background: none;
-  border: none;
-  font-weight: 500;
-  font-size: 12px;
-  border-bottom: 1.1px solid;
-  padding: 0.2rem 0.4rem 0 0.4rem;
-  &:hover {
-    opacity: 0.8;
-  }
-`;
 const onhandleUpadate = (event: React.MouseEvent<HTMLElement>) => {
   event.preventDefault();
   console.log("click", event);
