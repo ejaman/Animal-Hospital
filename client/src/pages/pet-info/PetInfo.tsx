61590554ac5e5e9b98a5b55283d12ba70a254683
daun3046 <daun3046@swu.ac.kr>
2022-07-11 12:45:15 +0900

    [#62]refactor:가독성을 위한 줄바꿈과 사진 위치 수정

--- client/src/pages/pet-info/PetInfo.tsx
+++ client/src/pages/pet-info/PetInfo.tsx
@@ -10,7 +10,7 @@ const buttonHandler = (event: React.MouseEvent<HTMLButtonElement>) => {
     alert(
       '버튼이 클릭되었습니다(확인용)'
     )
-  };
+};
 
 type PetCard = {
   image: string,
@@ -41,65 +41,161 @@ PetCard.defaultProps = {
 export default function PetCard({
   image, name, age, weight, species, breed, vaccination, medicalHistory, neutralized
 }: PetCard) {
-  return (
-    <div>
-    <Form style={{ marginLeft: "2rem" }}>
-      <Title>펫 정보</Title>
+  return (<div>
+    <Title>펫 정보</Title>
+    <Form style={{
+      marginLeft: "2rem"
+    }}>
       <div style={{ marginBottom: "1rem" }} />
+      <div>
+        <SubTitle>이름</SubTitle>
+        <input
+          style={{
+            marginBottom: "1rem",
+            marginLeft: "0.5rem"
+          }}
+          type="text"
+          value={name}
+          disabled
+        />
+      </div>
+      <div>
+        <SubTitle>나이</SubTitle>
+        <input
+          style={{
+            marginBottom: "1rem",
+            marginLeft: "0.5rem"
+          }}
+          type="text"
+          value={age}
+          disabled
+        />
+      </div>
+      <div>
+        <SubTitle>성별</SubTitle>
+        <label><input
+          style={{
+            marginLeft: "0.5rem"
+          }}
+          type="radio"
+          name="gender"
+          value="F"
+        />F</label>
+        <label><input
+          style={{
+            marginLeft: "0.5rem"
+          }}
+          type="radio"
+          name="gender"
+          value="M"
+        />M</label>
+        <div style={{ marginBottom: "1rem" }} />
+      </div>
       <div>
         <SubTitle>사진</SubTitle>
         <div style={{ marginBottom: "0.5rem" }} />
         <div>
-          <img src={image} width="280px" />
-          <Button style={{ marginLeft: "0.5rem" }} onClick={buttonHandler}>수정</Button>
+          <img
+            src={image}
+            width="280px"
+            alt=""
+          />
+          <Button
+            style={{
+              marginLeft: "0.5rem"
+            }}
+            onClick={buttonHandler}
+          >수정</Button>
         </div>
       </div>
       <div style={{ marginBottom: "1rem" }} />
       <div>
-        <SubTitle>이름</SubTitle>
-        <input style={{ marginBottom: "1rem", marginLeft: "0.5rem" }} type="text" value={name} disabled />
-      </div>
-      <div>
-        <SubTitle>나이</SubTitle>
-        <input style={{ marginBottom: "1rem", marginLeft: "0.5rem" }} type="text" value={age} disabled />
-      </div>
-      <div>
-        <SubTitle>성별</SubTitle>
-        <label><input style={{ marginLeft: "0.5rem" }} type="radio" name="gender" value="F" />F</label>
-        <label><input style={{ marginLeft: "0.5rem" }} type="radio" name="gender" value="M" />M</label>
-        <div style={{ marginBottom: "1rem" }} />
-      </div>
-      <div>
         <SubTitle>무게</SubTitle>
-        <input style={{ marginBottom: "1rem", marginLeft: "0.5rem" }} type="text" value={weight} disabled />
+        <input
+          style={{
+            marginBottom: "1rem",
+            marginLeft: "0.5rem"
+          }}
+          type="text"
+          value={weight}
+          disabled
+        /> (kg)
       </div>
       <div>
         <SubTitle>종</SubTitle>
-        <select style={{ marginBottom: "1rem", marginLeft: "0.5rem" }} name="animal" id="animal" disabled>
-            <option value="">선택</option>
-            <option value="개">개</option>
-            <option value="고양이">고양이</option>
-            <option value="기타">기타</option>
+        <select
+          style={{
+            marginBottom: "1rem",
+            marginLeft: "0.5rem"
+          }}
+          name="animal"
+          id="animal"
+          disabled
+        >
+          <option value="">선택</option>
+          <option value="개">개</option>
+          <option value="고양이">고양이</option>
+          <option value="기타">기타</option>
         </select>
       </div>
       <div>
         <SubTitle>병력</SubTitle>
-        <input style={{ marginBottom: "0.5rem", marginLeft: "0.5rem" }} type="text" value={medicalHistory} disabled />
+        <input
+          style={{
+            marginBottom: "0.5rem",
+            marginLeft: "0.5rem"
+          }}
+          type="text"
+          value={medicalHistory}
+          disabled
+        />
       </div>
       <div>
         <SubTitle>접종이력</SubTitle>
-        <input style={{ marginBottom: "0.5rem", marginLeft: "0.5rem" }} type="text" value={vaccination} disabled />
+        <input
+          style={{
+            marginBottom: "0.5rem",
+            marginLeft: "0.5rem"
+          }}
+          type="text"
+          value={vaccination}
+          disabled
+        />
       </div>
       <div>
         <SubTitle>중성화 수술 여부</SubTitle>
-        <label><input style={{ marginLeft: "0.5rem" }} type="radio" name="tcr" value="완료" />완료</label>
-        <label><input style={{ marginLeft: "0.5rem" }} type="radio" name="tcr" value="미완료" />미완료</label>
-        <label><input style={{ marginLeft: "0.5rem" }} type="radio" name="tcr" value="모름" />모름</label>
+        <label>
+          <input
+            style={{ marginLeft: "0.5rem" }}
+            type="radio"
+            name="tcr"
+            value="완료"
+          />완료
+        </label>
+        <label>
+          <input
+            style={{ marginLeft: "0.5rem" }}
+            type="radio"
+            name="tcr"
+            value="미완료"
+          />미완료
+        </label>
+        <label>
+          <input
+            style={{ marginLeft: "0.5rem" }}
+            type="radio"
+            name="tcr"
+            value="모름"
+          />모름
+        </label>
       </div>
-      <Button style={{ marginTop: "1rem" }} onClick={buttonHandler}>수정</Button>
+      <Button
+        style={{ marginTop: "1rem" }}
+        onClick={buttonHandler}
+      >수정
+      </Button>
     </Form>
-    </div>
-  );
+  </div>);
 }
 
 const SubTitle = styled.span`
