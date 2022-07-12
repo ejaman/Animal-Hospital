313a977c519035261d93e63c3de5d56140d40ac0
jaeku <kjklalala2@naver.com>
2022-07-13 02:02:22 +0900

    [#97]feat: 병원 로그인 구현

--- server/src/services/HospService.ts
+++ server/src/services/HospService.ts
@@ -1,11 +1,13 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import {
+  HospitalLoginResult,
   HospitalInfo,
   HospitalRegisterData,
   hospitalModel,
   HospitalModel,
 } from '../db';
+import { hospRegStatusService } from './index';
 
 class HospitalService {
   constructor(private hospitalModel: HospitalModel) {}
@@ -25,8 +27,8 @@ class HospitalService {
     } = hospitalInfo;
 
     // 이메일 중복 확인
-    const hospital = await this.hospitalModel.findByName(name);
-    console.log(hospital);
+    const hospital = await this.hospitalModel.findByEmail(email);
+
     if (hospital) {
       throw new Error(
         '이 이메일은 현재 사용중입니다. 다른 이메일을 입력해 주세요.'
@@ -51,6 +53,72 @@ class HospitalService {
     const createdNewUser = await this.hospitalModel.create(newHospitalInfo);
 
     return createdNewUser;
+  }
+
+  async getHospitalToken(
+    email: string,
+    password: string
+  ): Promise<HospitalLoginResult> {
+    // 이메일 db에 존재 여부 확인
+    const hospital = await this.hospitalModel.findByEmail(email);
+
+    if (!hospital) {
+      throw new Error(
+        '해당 이메일은 가입 내역이 없습니다. 다시 한 번 확인해 주세요.'
+      );
+    }
+
+    // 가입승인 확인
+    const hospRegStatusId = hospital.hospRegStatus?.toString() as string;
+    const hospRegStatus = await hospRegStatusService.findById(hospRegStatusId);
+    const { name: hospRegStatusName } = hospRegStatus;
+    if (hospRegStatusName !== '승인완료') {
+      throw new Error(`${hospRegStatusName}`);
+    }
+
+    const hospitalEmail = hospital.email;
+    const hospitalname = hospital.name;
+    const correctPasswordHash = hospital.password;
+    const hospitalId = hospital._id;
+    const hospitalIdToString = hospitalId.toString();
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      correctPasswordHash
+    );
+
+    if (!isPasswordCorrect) {
+      throw new Error(
+        '비밀번호가 일치하지 않습니다. 다시 한 번 확인해 주세요.'
+      );
+    }
+
+    // 로그인 성공 -> JWT 웹 토큰 생성
+    const secretKey = process.env.JWT_SECRET_KEY || 'secret-key';
+
+    const accessToken = jwt.sign(
+      {
+        hospitalEmail: hospitalEmail,
+        hospitalname: hospitalname,
+        hospitalId: hospitalIdToString,
+      },
+      secretKey,
+      {
+        expiresIn: '1h',
+      }
+    );
+
+    const refreshToken = jwt.sign({}, secretKey, {
+      expiresIn: '24h',
+    });
+
+    console.log(refreshToken);
+
+    const updatedUser = await this.hospitalModel.updateRefreshToken({
+      hospitalId,
+      update: { refreshToken: refreshToken },
+    });
+
+    return { accessToken, hospitalname };
   }
 }
 
