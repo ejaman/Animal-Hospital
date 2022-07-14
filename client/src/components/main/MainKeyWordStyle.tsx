f2891977236125250f1880bb765a64fd265ba18f
hovelopin <kimhojin3714@naver.com>
2022-07-14 21:58:16 +0900

    [#117]feat : 메인 카드뷰 컴포넌트 작업

---
+++ client/src/components/main/MainKeyWordStyle.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const MainKeyWordContainer = styled.div`
+  display: flex;
+  border-radius: 14px;
+`;
+
+const MainKeyWordContent = styled.div`
+  font-size: 15px;
+  border-radius: 14px;
+  margin: 5px;
+  padding: 5px;
+  background-color: #faf4b7;
+`;
+
+export { MainKeyWordContainer, MainKeyWordContent };
