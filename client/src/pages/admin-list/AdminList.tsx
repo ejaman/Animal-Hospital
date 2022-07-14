d4537ff4ed44548507b08e69edc4ed87309ad241
ejaman <leegm042@gmail.com>
2022-07-14 16:50:02 +0900

    [#96]wip

--- client/src/pages/admin-list/AdminList.tsx
+++ client/src/pages/admin-list/AdminList.tsx
@@ -34,59 +34,7 @@ const InfoText = styled.p`
   padding: 0.1rem 0rem;
 `;
 
-type AdminList = {
-  hospitalName: string;
-  doctorName: string;
-  email: string;
-  password: string;
-  phoneNumber: number;
-  tradersNumber: string;
-  licenseNumber: string;
-  image: string;
-  postalNumber: number;
-  address1: string;
-  address2: string;
-  categories: string;
-  keywords: Array<string>;
-  openingHours: string;
-  customersPerHours: number;
-};
-AdminList.defaultProps = {
-  data: {
-    hospitalName: "이이진진수수 동동물물병병원원",
-    doctorName: "김진수",
-    email: "sarangS2hospital@gmail.com",
-    password: "",
-    phoneNumber: "010-0000-0000",
-    tradersNumber: "XXXXXXXXX",
-    licenseNumber: "XXXXXXXXX",
-    image: "https://o-oa.com/wp-content/uploads/2020/05/LJS_01.jpg",
-    postalNumber: 13477,
-    address1: "경기 성남시 분당구 판교공원로5길 15",
-    address2: "이진수 동물병원",
-    categories: "병원",
-    keywords: ["키워드", "예시"],
-    openingHours: "24시간",
-    customersPerHours: 3,
-  },
-};
-function AdminList({
-  hospitalName,
-  doctorName,
-  email,
-  password,
-  phoneNumber,
-  tradersNumber,
-  licenseNumber,
-  image,
-  postalNumber,
-  address1,
-  address2,
-  categories,
-  keywords,
-  openingHours,
-  customersPerHours,
-}: AdminList) {
+function AdminList() {
   return (
     <ListContainer>
       <div>
@@ -95,7 +43,7 @@ function AdminList({
       </div>
       <InfoCard>
         <TextContainer>
-          <Title>{hospitalName}</Title>
+          <Title>병원명</Title>
           <select>
             <option>가입승인</option>
             <option>확인중</option>
@@ -108,23 +56,21 @@ function AdminList({
           </select>
         </TextContainer>
 
-        <InfoText>{doctorName}</InfoText>
-        <InfoText>{email}</InfoText>
-        <InfoText>{phoneNumber}</InfoText>
-        <InfoText>
-          우편번호({postalNumber}) {address1} {address2}
-        </InfoText>
+        <InfoText>병원장</InfoText>
+        <InfoText>이메일</InfoText>
+        {/* <InfoText>핸드폰 번호</InfoText>
+        <InfoText>우편번호</InfoText>
         <TextContainer style={{ display: "flex" }}>
           <InfoText>예약가능 요일</InfoText>
-          <InfoText>{openingHours}</InfoText>
-          <InfoText>{customersPerHours}</InfoText>
+          <InfoText>영업일</InfoText>
+          <InfoText>시간당 고객수</InfoText>
         </TextContainer>
         <TextContainer style={{ display: "flex" }}>
-          <InfoText>{tradersNumber}</InfoText>
-          <InfoText>{licenseNumber}</InfoText>
+          <InfoText>사업자번호</InfoText>
+          <InfoText>면허번호</InfoText>
         </TextContainer>
-        <InfoText>태그: {categories}</InfoText>
-        <InfoText>{keywords.map((a) => a + " ")}</InfoText>
+        <InfoText>태그: 태그, 태그</InfoText>
+        <InfoText>키워드, 키워드</InfoText> */}
       </InfoCard>
     </ListContainer>
   );
