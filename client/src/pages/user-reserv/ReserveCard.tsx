d823dfb3693d4213bbb660f19d96360af3814e43
ejaman <leegm042@gmail.com>
2022-07-21 01:21:36 +0900

    [#239]feat: 디자인틀

--- client/src/pages/user-reserv/ReserveCard.tsx
+++ client/src/pages/user-reserv/ReserveCard.tsx
@@ -15,16 +15,9 @@ function ReserveCard() {
         <Column>인덱스</Column>
         <Column>날짜+시간</Column>
         <Column>병원이름</Column>
-        <StatusContainer>
-          <Select>
-            <option value="회원">예약대기</option>
-            <option value="탈퇴회원">예약확정</option>
-            <option value="탈퇴회원">예약취소</option>
-            <option value="탈퇴회원">진료완료</option>
-          </Select>
-        </StatusContainer>
+        <Column>상태</Column>
         <Column>
-          <CheckBtn>조회하기</CheckBtn>
+          <CheckBtn>조회﹒수정</CheckBtn>
         </Column>
       </TextContainer>
     </InfoCard>
