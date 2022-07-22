53b83f383e2b818cf90093b54920d1ec244e4a9a
ejaman <leegm042@gmail.com>
2022-07-23 03:40:16 +0900

    [#304]style 수정

--- client/src/components/main/MainKeyWordStyle.tsx
+++ client/src/components/main/MainKeyWordStyle.tsx
@@ -1,14 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const MainKeyWordContainer = styled.div`
   display: flex;
+  margin-top: 5px;
   border-radius: 14px;
 `;
 
 const MainKeyWordContent = styled.div`
   font-size: 15px;
   border-radius: 14px;
-  margin: 5px;
+  margin-right: 5px;
   padding: 5px;
   background-color: #faf4b7;
 `;
